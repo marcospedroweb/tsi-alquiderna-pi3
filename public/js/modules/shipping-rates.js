51b83b3335cda43cc7e092c0bed6be5a4a0a56e9
Marcos <king.frontweb@outlook.com>
2022-05-21 16:13:33 -0300

    Fetch de cep em desenvolvilmento

--- public/js/modules/shipping-rates.js
+++ public/js/modules/shipping-rates.js
@@ -4,44 +4,45 @@ export default function initShippingRates() {
 
 }
 
-let found;
-
-function checkChar(event) {
-    const char = String.fromCharCode(event.keyCode);
-    const patternWithout = '[0-9-]';
-
-
-    let cont = 0;
-
-    console.log(inputCep.value.indexOf('-'))
-    do {
-        found = inputCep.value.indexOf('-');
-        if (found !== -1)
-            cont++;
-    } while (found === -1);
-
-    // if ()
-    //     inputCep.value = inputCep.value.replace('-', '');
-
-    if (!char.match(patternWithout) || inputCep.value.length > 8) {
-        event.preventDefault();
-    }
-}
-
 const inputCep = document.querySelector('#input-cep');
 const btnSearchCep = document.querySelector('#search-cep');
 const divCep = document.querySelector('#div-cep');
+
 if (inputCep && btnSearchCep && divCep) {
+
+    //Validation
+    inputCep.addEventListener('input', event => {
+        if (event.inputType === 'insertFromPaste')
+            inputCep.value = '';
+    });
+    //Validation
     inputCep.addEventListener('keypress', event => {
-        checkChar(event);
+        if (inputCep.value.length > 7 && inputCep.value.indexOf('-') === -1)
+            // without ' - '
+            event.preventDefault();
+        else if (inputCep.value.length > 8 && inputCep.value.indexOf('-') !== -1)
+            // with ' - '
+            event.preventDefault();
+        else if ((event.key === '-' && inputCep.value.indexOf('-') !== -1))
+            event.preventDefault();
+        else if (isNaN(event.key) && event.key !== '-')
+            event.preventDefault();
     });
 
+    async function initFetchCep(cep) {
+        const promisseCep = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+        return promisseCep;
+    }
+
+    //Search
     btnSearchCep.addEventListener('click', event => {
-        if (inputCep.value.length && inputCep.value.length >= 8) {
-            if (inputCep.value.indexOf(/[@!#$%^&*()/\\_]/))
-                console.log('teste');
-            else
-                console.log('não')
+        if (inputCep.value.replace('-', '').length && inputCep.value.replace('-', '').length === 8) {
+            const promisseCep = initFetchCep(inputCep.value.replace('-', ''));
+            promisseCep
+                .then(response => response.json())
+                .then(cepJson => {
+                    console.log(cepJson);
+                });
         } else {
             initAlertUser('danger', 'Adicione um CEP válido.');
         }
--- resources/views/components/auth-validation-errors.blade.php
+++ resources/views/components/auth-validation-errors.blade.php
@@ -9,15 +9,30 @@
             <div>
                 @foreach ($errors->all() as $error)
                     @if ($error === 'The password confirmation does not match.')
-                        <span class="mt-3">A senhas não são iguais.</span>
+                        <div>
+                            <span class="mt-3">A senhas não são iguais.</span>
+                        </div>
                     @elseif ($error === 'These credentials do not match our records.')
-                        <span class="mt-3">O email e/ou senha não correspondem.</span>
-                    @elseif ($error === 'The password must contain at least one uppercase and one lowercase letter')
-                        <span class="mt-3">Sua senha deve ter ao menos letra minúsculas e uma maiúscula.</span>
+                        <div>
+                            <span class="mt-3">O email e/ou senha não correspondem.</span>
+                        </div>
+                    @elseif ($error === 'The password must contain at least one uppercase and one lowercase letter.')
+                        <div>
+                            <span class="mt-3">Sua senha deve ter ao menos letra minúsculas e uma
+                                maiúscula.</span>
+                        </div>
                     @elseif ($error === 'The password requires at least one number.')
-                        <span class="mt-3">A senha deve ter ao menos um numero.</span>
+                        <div>
+                            <span class="mt-3">A senha deve ter ao menos um numero.</span>
+                        </div>
+                    @elseif ($error === 'The email has already been taken.')
+                        <div>
+                            <span class="mt-3">Este email não está disponível.</span>
+                        </div>
                     @else
-                        <span class="mt-3">{{ $error }}</span>
+                        <div>
+                            <span class="mt-3">{{ $error }}</span>
+                        </div>
                     @endif
                 @endforeach
             </div>
