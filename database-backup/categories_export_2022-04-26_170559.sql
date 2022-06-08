da6cf136f791b3644fbcc65f86724bb43e8eb340
Marcos <king.frontweb@outlook.com>
2022-06-07 22:19:46 -0300

    Att total

--- database-backup/categories_export_2022-04-26_170559.sql
+++
@@ -1,5 +0,0 @@
-insert into `categories` (`created_at`, `deleted_at`, `id`, `name`, `updated_at`) values (NULL, NULL, '1', 'Armadura', '2022-04-01 03:37:32');
-insert into `categories` (`created_at`, `deleted_at`, `id`, `name`, `updated_at`) values ('2022-04-01 03:37:53', NULL, '2', 'Arma física', '2022-04-01 03:37:53');
-insert into `categories` (`created_at`, `deleted_at`, `id`, `name`, `updated_at`) values ('2022-04-01 03:38:02', NULL, '3', 'Arma mágica', '2022-04-01 03:38:02');
-insert into `categories` (`created_at`, `deleted_at`, `id`, `name`, `updated_at`) values ('2022-04-01 03:38:10', NULL, '4', 'Poção', '2022-04-01 03:38:10');
-insert into `categories` (`created_at`, `deleted_at`, `id`, `name`, `updated_at`) values ('2022-04-01 03:38:17', NULL, '5', 'Grimório', '2022-04-02 01:35:20');
--- public/js/modules/hide-alert.js
+++
@@ -1,8 +0,0 @@
-export default function initHideAlert() {
-    const alert = document.querySelector('#div-alert-flash');
-    if (alert)
-        setTimeout(() => {
-            if (!alert.classList.contains('d-none'))
-                alert.classList.toggle('d-none');
-        }, 3000);
-}
