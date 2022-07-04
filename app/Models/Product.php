4d8371a3fa8179385675bb8467c618c50ab3a14c
Marcos <king.frontweb@outlook.com>
2022-07-04 19:58:30 -0300

    Softdeletes

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,14 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\softDeletes;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 use Illuminate\Support\Facades\DB;
 
 class Product extends Model
 {
     use HasFactory;
-    use softDeletes;
+    use SoftDeletes;
     protected $fillable = [
         'name',
         'description',
